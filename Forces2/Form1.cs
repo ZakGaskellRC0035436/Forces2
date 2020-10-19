81d487aef56549522f567dc4379df9c749e5569f
Zak Gaskell <rc0035436@student.riversidecollege.ac.uk>
2020-10-19 15:14:57 +0100

    kdgjkdf

--- Forces2/Form1.cs
+++ Forces2/Form1.cs
@@ -16,5 +16,72 @@ namespace Forces2
         {
             InitializeComponent();
         }
+        Double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        Double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+        }
+        Double tan(double x)
+        {
+            return (Math.Tan(x * Math.PI / 180.0));
+        }
+
+        double atan(double x)
+        {
+            return (Math.Atan(x) * 180 / Math.PI);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Force = double.Parse(textBox1.Text);
+                double Angle = double.Parse(textBox2.Text);
+
+                double Fx = Force * cos(Angle);
+                double Fy = Force * sin(Angle);
+
+                label1.Text = "Fx = " + Fx;
+                label2.Text = "Fy = " + Fy;
+            }
+            catch
+            {
+                MessageBox.Show("Error on imput");
+            }
+
+        }
     }
 }
